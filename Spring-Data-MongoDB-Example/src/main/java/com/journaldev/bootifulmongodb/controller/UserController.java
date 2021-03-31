11ecd856cd45dfef9afb16276089f46098735671
Sanjay Singh Solanki <singh.solanki80@gmail.com>
2021-03-31 13:18:14 +0530

    Comment Removed from controller

--- Spring-Data-MongoDB-Example/src/main/java/com/journaldev/bootifulmongodb/controller/UserController.java
+++ Spring-Data-MongoDB-Example/src/main/java/com/journaldev/bootifulmongodb/controller/UserController.java
@@ -67,19 +67,7 @@ public class UserController {
 		}
 	}
 
-	// @RequestMapping(value = "/settings/{userId}/{key}", method =
-	// RequestMethod.GET)
-	// public String getUserSetting(@PathVariable String userId, @PathVariable
-	// String key) {
-	// //User user = userRepository.findOne(userId);
-	// String setting = userDAL.getUserSetting(userId, key);
-	// LOG.info("Setting = "+setting);
-	// if (setting != null) {
-	// return setting;
-	// } else {
-	// return "Setting not found.";
-	// }
-	// }
+	//Comment removed from here
 
 	@RequestMapping(value = "/settings/{userId}/{key}", method = RequestMethod.GET)
 	public String getUserSetting(@PathVariable String userId, @PathVariable String key) {
